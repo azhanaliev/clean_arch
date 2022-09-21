bb2f13e3126fd1a2b7ace4c066c5e152e0ed892c
Azamat <aza@MacBook-Pro-Azamat.local>
2022-09-21 18:28:40 +0600

    2nd

--- lib/feature/data/data_sources/person_local_data_source.dart
+++ lib/feature/data/data_sources/person_local_data_source.dart
@@ -1,7 +1,37 @@
+import 'dart:convert';
+
+import 'package:clean_arch/core/error/exception.dart';
 import 'package:clean_arch/feature/data/models/person_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class PersonLocalDataSource {
   Future<List<PersonModel>> getLastPersonsFromCache();
 
   Future<void> personsToCache(List<PersonModel> persons);
+}
+
+const cachedPersonsList = 'CACHED_PERSONS_LIST';
+
+class PersonLocalDataSourceImpl implements PersonLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  PersonLocalDataSourceImpl(this.sharedPreferences);
+
+  @override
+  Future<List<PersonModel>> getLastPersonsFromCache() async {
+    final jsonPersonsList = sharedPreferences.getStringList(cachedPersonsList);
+    if (jsonPersonsList != null) {
+      return Future.value(jsonPersonsList.map((e) => null))
+    } else {
+      CacheException();
+    }
+  }
+
+  @override
+  Future<void> personsToCache(List<PersonModel> persons) async {
+    final List<String> jsonPersonsList =
+        persons.map((person) => json.encode(person.toJson())).toList();
+    sharedPreferences.setStringList(cachedPersonsList, jsonPersonsList);
+    Future.value(jsonPersonsList);
+  }
 }
